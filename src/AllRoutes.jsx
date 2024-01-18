04b584b61d595db09062aeb521a459c20a97bf2a
SGSANJAY044 <sgsanjay044@gmail.com>
2024-01-18 19:17:48 +0530

    Pages Layout build

---
+++ src/AllRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Login from './Pages/Login/Login'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import Browse from './Pages/ Browse/Browse'
+
+function AllRoutes() {
+    return (
+        <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/browse' element={<Browse/>}/>
+        </Routes>
+        )
+}
+
+export default AllRoutes
---
+++ src/Pages/ Browse/Browse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Browse() {
+    return (
+        <div>
+           Browse
+        </div>
+    )
+}
+
+export default Browse
