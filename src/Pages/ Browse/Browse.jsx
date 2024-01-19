037c98cc977491324ddfca4ba4a552de34bfff2b
SGSANJAY044 <sgsanjay044@gmail.com>
2024-01-19 15:46:03 +0530

    update existing code

--- src/Pages/ Browse/Browse.jsx
+++ src/Pages/ Browse/Browse.jsx
@@ -2,8 +2,46 @@ import React from 'react'
 
 function Browse() {
     return (
-        <div>
-           Browse
+    <div>
+    <header class="app-header">
+        <div class="app-header_container">
+            <p class="logo"><a href="index.html">DATABASE PROJECT</a></p>
+            <nav class="app-header_links">
+                <li><a href="browse.html">Browse</a></li>
+                <li><a href="login.html">Log in</a></li>
+                <li><a href="cart.html"><img class="cart" src="images/shopping-cart.png"/></a></li>
+            </nav>
+        </div>
+    </header>
+    
+    <div id="home" class="home">
+        <div class="home-wrapper">
+            <div class="home-intro">
+                <div class="browse-content">
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                    <div class="content-item"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="app-footer">
+        <div class="app-footer_container">
+            <p><a href="" target="_blank">Link</a></p>
+            <p><a href="" target="_blank">Link</a></p>
+            <p><a href="" target="_blank">Link</a></p>
+        </div>
+    </footer>
         </div>
     )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function Home() {
     return (
         <div>
-           Home
+           <header class="app-header">
+        <div class="app-header_container">
+            <p class="logo"><a href="#home">DATABASE PROJECT</a></p>
+            <nav class="app-header_links">
+                <li><a href="browse.html">Browse</a></li>
+                <li><a href="login.html">Log in</a></li>
+                <li><a href="cart.html"><img class="cart" src="images/shopping-cart.png"/></a></li>
+            </nav>
+        </div>
+    </header>
+    
+    <div id="home" class="home">
+        <div class="home-wrapper">
+            <div class="home-intro">
+                <div class="home-intro-content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi odit, ipsa numquam velit dicta est cupiditate non impedit error magnam necessitatibus explicabo. Magnam voluptatum odio maiores, atque laudantium minus tempore!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="app-footer">
+        <div class="app-footer_container">
+            <p><a href="" target="_blank">Link</a></p>
+            <p><a href="" target="_blank">Link</a></p>
+            <p><a href="" target="_blank">Link</a></p>
+        </div>
+    </footer>
         </div>
     )
 }
