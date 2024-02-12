3c4084edbab91e95f53bf61356afbef08fb078b8
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2024-02-12 21:19:19 +0100

    filter functionality first version
    filtering items by category now works, but can only filter by one category at a time

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -12,32 +12,68 @@ import Footer from "../Components/Footer";
 import "../stylesheets/navbar.css";
 import "../stylesheets/footer.css";
 
-import "../Components/DropdownMenu";
-
 const Browse = () => {
   const [products, setProducts] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [fileredProductList, setFilteredProductList] = useState([]);
+
+  // make prettier by not needing .value
+  const addFilterCategory = (category) => {
+    if(!selectedCategories.includes(category.value)){
+      setSelectedCategories(prev => ([...prev, category.value]))
+      console.log("added:", category.value);
+    }
+  };
+
+  // add functionality to only clear a certain category
+  const resetCategory = () => {
+    setSelectedCategories([]);
+  }
+  
+  // add functionality to be able to filter multiple categories at once
+  useEffect(() => {
+    if(selectedCategories.length === 0){
+      setFilteredProductList(products);
+      console.log("default page");
+    } else{
+      setFilteredProductList(products.filter((item) => (selectedCategories.includes(item.size))));
+      console.log("filtered page");
+    }
+  }, [selectedCategories, products]);
+
+  const fetchAllProducts = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800/browse");
+      setProducts(res.data);
+      setFilteredProductList(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    const fetchAllProducts = async () => {
-      try {
-        const res = await axios.get("http://localhost:8800/browse");
-        setProducts(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     fetchAllProducts();
   }, []);
 
+  // Enables clickable products
   const navigate = useNavigate();
-
   const handleClick = (id) => async (e) => {
     e.preventDefault();
     try {
       await navigate(`/product`, { state: { pid: id } });
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  // add functionality to only clear a certain category
+  const handleSelectChange = (selectedOptions) => {
+    if(selectedOptions.length === 0) {
+      resetCategory();
+      console.log("cleared");
+    } else {
+      console.log("changed:", selectedOptions);
+      selectedOptions.forEach(addFilterCategory)
     }
   };
 
@@ -48,9 +84,9 @@ const Browse = () => {
   ];
 
   const brands = [
-    { label: 'Small', value: 'S' },
-    { label: 'Medium', value: 'M' },
-    { label: 'Large', value: 'L' }
+    { label: 'Brand1', value: 'B1' },
+    { label: 'Brand2', value: 'B2' },
+    { label: 'Brand3', value: 'B3' }
   ];
 
   return (
@@ -58,11 +94,11 @@ const Browse = () => {
       <Navbar />
       <div className="browse-container">
         <div className="browse-filter">
-          <Select options={sizes} isMulti placeholder="Size" className="browse-select"/>
-          <Select options={brands} isMulti placeholder="Brand" className="browse-select"/>
+          <Select options={sizes} onChange={handleSelectChange} isMulti placeholder="Size" className="browse-select"/>
+          <Select options={brands} onChange={handleSelectChange} isMulti placeholder="Brand" className="browse-select"/>
         </div>
         <div className="browse-products">
-          {products.map((product) => (
+          {fileredProductList.map((product) => (
             <div
               className="browse-product"
               onClick={handleClick(product.idProduct)}
