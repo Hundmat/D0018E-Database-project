a735e1e2a6a63ab52c1257731a79cb97fd72e5c0
Anton Follinger <47656465+Hundmat@users.noreply.github.com>
2024-02-21 09:50:29 +0100

    Started to fix order

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,12 @@ import Product from './pages/Product';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Cart from './pages/Cart';
+<<<<<<< Updated upstream
 import Profile from './Components/Profile';
+=======
+import Order from './pages/Order';
+
+>>>>>>> Stashed changes
 // Stylesheets
 import "./stylesheets/style.css";
 
@@ -23,6 +28,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
+<<<<<<< Updated upstream
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
           <Route path="/add" element={<Add />} />
@@ -30,6 +36,16 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/profile" element={<Profile />} />
+=======
+          <Route path="/" element={<Home/>}/>
+          <Route path="/browse" element={<Browse/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/product" element={<Product/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/order" element={<Order/>}/>
+>>>>>>> Stashed changes
         </Routes>
       </BrowserRouter>
     </div>
