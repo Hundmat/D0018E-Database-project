8c856da6287036fc9705277c59e4396716c5eb04
Anton Follinger <47656465+Hundmat@users.noreply.github.com>
2024-02-26 13:05:23 +0100

    fixed navigate in order

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -182,8 +182,7 @@ const Order = () => {
         cvv: ''
       });
       setFormCart([]);
-      alert('Order submitted successfully');
-      navigate("/home");
+      navigate("/");
 
 
     } catch (error){
